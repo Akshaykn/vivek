e6907adeb9e6fc098435d67959559f2424610842
akshay <akshaykrishna633@gmail.com>
2018-07-16 01:01:45 +0530

    First commit

--- index.js
+++ index.js
@@ -1,67 +1,49 @@
 
 import React, { Component } from 'react';
-import { AppRegistry,View,StyleSheet,Text,ViewPagerAndroid,Image} from 'react-native';
+import { AppRegistry,View,Text,FlatList,StyleSheet } from 'react-native';
 
 export default class Bananas extends Component {
-  
-
   render() {
     return (
-      <ViewPagerAndroid
-      style={styles.viewPager}
-      initialPage={0}>
-      <View style={styles.pageStyle} key="1">
-        <Text>First page</Text>
+      <View style={styles.container}>
+           <FlatList
+             data = {[
+                            {key : 'Akshay'},
+                            {key : 'Mani'},
+                            {key : 'Naveen'},
+                            {key : 'Gautam'},
+                            {key : 'Alok'},
+                            {key : 'Sachin'},
+                            {key : 'Akshay'},
+                            {key : 'Mani'},
+                            {key : 'Naveen'},
+                            {key : 'Gautam'},
+                            {key : 'Alok'},
+                            {key : 'Sachin'},
+                            {key : 'Akshay'},
+                            {key : 'Mani'},
+                            {key : 'Naveen'},
+                            {key : 'Gautam'},
+                            {key : 'Alok'},
+                            {key : 'Sachin'}
+                     ]}
+
+             renderItem = {  ({ item }) => < Text style = { styles.item }>   { item.key } </Text>    }
+           />
       </View>
-      <View style={styles.pageStyle} key="2">
-        <Text>Second page</Text>
-      </View>
-      <View style={styles.pageStyle} key="3">
-        <Text>Third page</Text>
-      </View>
-      <View style={styles.pageStyle} key="4">
-        <Text>Fourth page</Text>
-      </View>
-    </ViewPagerAndroid>
-     
     );
   }
 }
    styles =  StyleSheet.create({
-     
-    container:{
-      paddingTop: 60,
-      alignItems: 'center'
-    },
-    buttoncontainer:{
-      margin:20
-    },
-    button: {
-      marginBottom: 30,
-      width: 200,
-      alignItems: 'center',
-      backgroundColor: '#2196F3',
-      margin:40
-    },
-    buttonText: {
-      padding: 20,
-      color: 'white'
-    },
-    viewPager: {
-      flex: 1
-    },
-    pageStyle: {
-      alignItems: 'center',
-      padding: 20,
-    }
-    
+    container : {
+      flex:       1,
+      paddingTop: 22
+                },
+    item :      {
+      padding:  10,
+      fontSize: 20,
+      height:   44,
+      color:    'black'
+                }
    })
-   
-  
-
-
-  
-
-
-
 AppRegistry.registerComponent('vivek', () => Bananas);
